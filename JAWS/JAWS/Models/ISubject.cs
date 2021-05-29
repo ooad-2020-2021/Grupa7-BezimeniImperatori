2ef4f46deb067e36220b82e0aeb4b74f277f0ae7
hisic1 <hisic1@etf.unsa.ba>
2021-05-29 17:15:32 +0200

    Dodat interface ISubject

---
+++ JAWS/JAWS/Models/ISubject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JAWS.Models
+{
+    interface ISubject //implementirat ce KorisnikController. Potrebno jos dodati RealSubject I Proxy, kako?
+    {
+        public void PromijeniEmail(string noviEmail);
+    }
+}
